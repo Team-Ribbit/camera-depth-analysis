767f3e31df5cfcb7aa4fbcceef57a5dd299d2b6e
Chad Paik <chaddy.dev@gmail.com>
2024-05-30 18:57:43 -0400

    Initial commit. Add some skeletal code

---
+++ camera.py
@@ -0,0 +1,19 @@
+class Camera():
+    def __init__(self, f):
+        self.f = f
+
+    def metre_to_pixel(self, z):
+        """
+        calculate how man pixels would 1 meter be given z distance
+        :param z:
+        :return:
+        """
+        pass
+
+    def _minimum_distance_to_meter(self):
+        """
+        Calculate the minimum distance to see 1 meter based on the current field of view
+        :return:
+        """
+        pass
+
